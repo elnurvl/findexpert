55b4fc982a38c81e5aba97f1282d5e735b7df1e8
Elnur Hajiyev <elnurvl@gmail.com>
2021-04-01 01:05:16 +0400

    Fire necessary events after the registration
    - Website should be parsed to pull the headings
    - User profile should be updated with the shortened URL.

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -2,9 +2,13 @@
 
 namespace Tests\Feature;
 
+use App\Listeners\PullHeadings;
+use App\Listeners\ShortenUrl;
 use App\Providers\RouteServiceProvider;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Event;
 use Laravel\Jetstream\Jetstream;
 use Tests\TestCase;
 
@@ -21,8 +25,11 @@ class RegistrationTest extends TestCase
 
     public function test_new_users_can_register()
     {
+        // Arrange
+        Event::fake();
         $url = 'http://devadamlar.com';
 
+        // Act
         $response = $this->post('/register', [
             'name' => 'Test User',
             'email' => 'test@example.com',
@@ -32,6 +39,9 @@ class RegistrationTest extends TestCase
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature(),
         ]);
 
+        // Assert
+        Event::assertListening(Registered::class, ShortenUrl::class);
+        Event::assertListening(Registered::class, PullHeadings::class);
         $this->assertAuthenticated();
         $this->assertEquals($url, Auth::user()->website);
         $response->assertRedirect(RouteServiceProvider::HOME);
