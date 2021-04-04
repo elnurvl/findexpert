edd503a616c5000d728393b6d1505a343e5b649c
Elnur Hajiyev <elnurvl@gmail.com>
2021-04-05 01:19:16 +0400

    Add missed fields to the UserResource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -20,7 +20,11 @@ class UserResource extends JsonResource
             'shortening' => $this->shortening,
             'total_friends' => $this->friends_count,
             'network' => $this->network != null ? UserResource::collection($this->network) : null,
-            'is_friend' => $this->is_friend ?? false
+            'is_friend' => $this->is_friend ?? false,
+            'picture' => $this->profile_photo_path ?? $this->profile_photo_url,
+            'no_topic' => $this->no_topic,
+            'failed_to_reach' => $this->failed_to_reach,
+            'registered_at' => (string) $this->created_at
         ];
     }
 }
